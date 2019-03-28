5e40a7664ef96da192d9805e40d789528e4990ed
Imandra McKenzie <indigoimandra@gmail.com>
2019-03-27 17:48:26 -0700

    Add initial checkbox form fields and partially functioning JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,36 @@
 // Business logic
+var pizza = new Pizza();
 
+function Pizza(){
+  this.toppings = [];
+}
+
+Pizza.prototype.allToppings = function(){
+  this.toppings.push(toppingInputs.get())
+}
 
 $(document).ready(function(){
+  $("form#pizza").click(function(event){
+    var toppingInputs = $("input:checkbox[name=topping]:checked").map(function(){
+      return $(this).val()
+    });
 
+    $("#result").text(toppingInputs.get())
+  });
 });
+
+/* $(document).ready(function(){
+  var tmp = [];
+  $("input").click(function() {
+    if ($(this).is(':checked')) {
+      var checked = ($(this).val());
+      tmp.push(checked);
+    } else {
+      tmp.splice($.inArray(checked, tmp),1);
+    }
+  });
+
+    $('#sub-btn').on('click', function () {
+      alert(tmp)
+    });
+}); */
