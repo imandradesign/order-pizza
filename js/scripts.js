98275b143c6a97793c559209cf6917fabf57e016
Imandra McKenzie <indigoimandra@gmail.com>
2019-03-29 16:00:35 -0700

    Update JS to calculate price based on size and add gradient header background

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,27 @@ function Pizza(){
   this.price = 0;
 }
 
-Pizza.prototype.fullOrder = function(){
-  return "<strong>Size: </strong>" + this.size + "<br><strong>Toppings: </strong>" + this.toppings + "<br><strong>Total: </strong>$" + this.price
+Pizza.prototype.isEmpty = function() {
+  if (this.toppings.length === 0){
+    alert("Please enter at least one topping");
+    $(".results").hide();
+  }
 }
 
-/* Pizza.prototype.totalCost = function(){
-  if (this.size === "Small"){
-    this.price += 10
-  } else if (this.size === "Medium"){
-    this.price += 15
-  } else if (this.size === "Large"){
-    this.price += 20
+Pizza.prototype.fullOrder = function(){
+  return "<span id='order'><strong>Size: </strong>" + this.size + "<br><strong>Toppings: </strong>" + this.toppings + "<br><strong>Total: </strong>$" + this.price + "</span>"
+}
+
+Pizza.prototype.totalCost = function(){
+  if ($("input:radio[name=size]:checked").val() === "Small"){
+    this.price = 10
+  } else if ($("input:radio[name=size]:checked").val() === "Medium"){
+    this.price = 15
+  } else if ($("input:radio[name=size]:checked").val() === "Large"){
+    this.price = 20
   }
-}; */
+};
+
 
 
 // User logic
@@ -34,11 +42,16 @@ $(document).ready(function(){
       pizza.toppings.push($(this).val());
     });
 
+    pizza.totalCost();
 
-    $("#order").append(pizza.fullOrder());
+    $("#order").remove();
+    $("#results").append(pizza.fullOrder());
     $(".results").show();
+
+    pizza.isEmpty();
 
     pizza.toppings = [];
     pizza.size = [];
+    this.price = 0;
   });
 });
